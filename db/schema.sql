7540a22558f3c70c4848b0ccc32212b88bcae5b9
Jose Contreras <10jplz@gmail.com>
2021-06-30 15:23:43 -0600

    Created Starter Files Using readme guide

---
+++ db/schema.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE  burger_db;
+
+USE burger_db;
+
+CREATE TABLE burgers(
+    id INT NOT NULL AUTO_INCREMENT,
+    PRIMARY KEY (id),
+    burger_name VARCHAR(30) NOT NULL,
+    devoured BOOLEAN,
+
+);
---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+
+INSERT INTO burgers (burger_name) VALUES ("JALAPEÑO BURGER");
+INSERT INTO burgers (burger_name) VALUES ("APOLLO BURGER");
+INSERT INTO burgers (burger_name) VALUE ("BIG MAC");
