f89a36a0701a19ddbec51653bceb967eed5e1649
Jose Contreras <10jplz@gmail.com>
2021-06-30 18:02:25 -0600

    Worked on ORM File

--- config/orm.js
+++ config/orm.js
@@ -1,1 +1,39 @@
+const { create } = require('express-handlebars');
 const connection = require('./connection');
+
+
+const orm = {
+    all(tableInput, cb) {
+        const queryString = `SELECT * FROM ${tableInput};`;
+        connection.query(queryString, (err, result) => {
+            if(err) {
+                throw err; 
+            }
+            cb(result);
+        });
+    },
+
+    create(table, data, cb) {
+        let queryString = `INSERT INTO ?? SET ?`;
+        console.log(queryString);
+        connection.query(queryString, vals, (err, result) => {
+            if (err) {
+                throw err; 
+            }
+
+            cb(result);
+        })
+
+    },
+    update(table, objColVals, condition, db) {
+        let queryString = `UPDATE ?? SET ? WHERE ?`;
+        console.log(queryString);
+        connection.query(queryString, (err, result) => {
+            if (err) {
+                trow err; 
+            }
+            cb(result)
+        })
+    }
+}
+module.exports = orm;
