3955f33bab0d566491ed426e7011195efc90e327
Jose Contreras <10jplz@Gmail.com>
2021-07-09 22:05:36 -0600

    Fixed connection file

--- config/connection.js
+++ config/connection.js
@@ -1,28 +1,27 @@
 // Set up MySQL connection.
 const mysql = require('mysql');
 
-const connection;
-if (process.env.JAWSDB_URL) {
-  connection = mysql.createConnection(process.env.JAWSDB_URL);
+let connection;
+
+if (process.env.JAWSDB_URL){
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
-  connection = mysql.createConnection({
-  host: 'localhost',
-  port: 3306,
-  user: 'root',
-  // NOTE: Be sure to add your MySQL password here!
-  password: '',
-  database: 'burger_db',
-});
+    const connection = mysql.createConnection({
+        host: 'localhost',
+        port: 3306,
+        user: 'root',
+        password: '',
+        database: 'burgers_db',
+    });
 }
 
-// Make connection.
+
 connection.connect((err) => {
-  if (err) {
-    console.error(`error connecting: ${err.stack}`);
-    return;
-  }
-  console.log(`connected as id ${connection.threadId}`);
+    if (err) {
+        console.log(`error connecting: ${err.stack}`);
+        return;
+    }
+    console.log(`connected as id ${connection.threadId}`);
 });
 
-// Export connection for our ORM to use.
 module.exports = connection;
